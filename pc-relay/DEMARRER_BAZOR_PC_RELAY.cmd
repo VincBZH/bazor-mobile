833956c69d2b8617d9b5acc692209aef01e0d976
VincBZH <126826856+VincBZH@users.noreply.github.com>
2026-09-16 01:35:11 +0200

    Fix Windows quoting in BAZOR Python validation command

--- pc-relay/DEMARRER_BAZOR_PC_RELAY.cmd
+++ pc-relay/DEMARRER_BAZOR_PC_RELAY.cmd
@@ -38,7 +38,7 @@ if errorlevel 1 (
 )
 
 echo [3/4] Verification du code BAZOR v3...
-python -c "import py_compile,sys,tempfile,os; [py_compile.compile(p,cfile=os.path.join(tempfile.gettempdir(),\"BAZOR_VALIDATE_\"+str(os.getpid())+\"_\"+str(i)+\".pyc\"),doraise=True) for i,p in enumerate(sys.argv[1:],1)]" "%~dp0mammouth_client.py" "%~dp0bazor_pc_relay_v3.py"
+python -c "import py_compile,sys,tempfile,os; [py_compile.compile(p,cfile=os.path.join(tempfile.gettempdir(),'BAZOR_VALIDATE_'+str(os.getpid())+'_'+str(i)+'.pyc'),doraise=True) for i,p in enumerate(sys.argv[1:],1)]" "%~dp0mammouth_client.py" "%~dp0bazor_pc_relay_v3.py"
 if errorlevel 1 (
   echo.
   echo [BLOQUE] Le controle Python a detecte un probleme.
